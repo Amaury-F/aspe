9ddd1f00663e000e37d1f2932831bf9f36fc36dc
Amaury <amaury.fauvel@etu.univ-rouen.fr>
2018-01-24 12:25:37 +0100

    test de la SFML. (c'est trop bien)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    
+    RenderWindow window(VideoMode(800, 600), "hey");
+    while (window.isOpen()) {
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear(Color::Black);
+
+
+
+
+        window.display();
+    }
     return 0;
 }
