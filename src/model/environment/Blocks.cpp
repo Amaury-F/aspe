50686e39068be174a6c09efdf160e517e7c74cba
delapsev <severine.delaplace@etu.univ-rouen.fr>
2018-03-16 17:50:41 +0100

    Gestion des blocks.

--- src/model/environment/Blocks.cpp
+++ src/model/environment/Blocks.cpp
@@ -3,3 +3,63 @@
 //
 
 #include "Blocks.h"
+#include "../ModelConstants.h"
+
+bool Blocks::isGround(block b) {
+    return b >= Blocks::GROUND;
+}
+
+bool Blocks::isWall(block b) {
+    return b >= Blocks::WALL;
+}
+
+bool Blocks::isUp(block b) {
+    return (b >= Blocks::GROUND_SLOPENW1 && b <= Blocks::GROUND_SLOPENE5)
+           || (b >= Blocks::WALL_SLOPENW1 && b <= Blocks::WALL_SLOPENE5);
+}
+
+bool Blocks::isDown(block b) {
+    return (b >= Blocks::GROUND_SLOPESW1 && b <= Blocks::GROUND_SLOPESE5)
+           || (b >= Blocks::WALL_SLOPESE1 && b <= Blocks::WALL_SLOPESE5);
+}
+
+bool Blocks::isSlope(block b) {
+    return Blocks::isUp(b) || Blocks::isDown(b);
+}
+int Blocks::slope(int x, block b) {
+    if (b == Blocks::GROUND_SLOPENW1 || b == Blocks::GROUND_SLOPESE1
+            || b == Blocks::WALL_SLOPENW1 || b == Blocks::WALL_SLOPESE1) {
+        return x;
+    } else if (b == Blocks::GROUND_SLOPENE1 || b == Blocks::GROUND_SLOPESW1
+            || b == Blocks::WALL_SLOPENE1 || b == Blocks::WALL_SLOPESW1) {
+        return BLOCK_SIZE - x;
+    } else if (b == Blocks::GROUND_SLOPENW3 || b == Blocks::GROUND_SLOPESE5
+            || b == Blocks::WALL_SLOPENW3 || b == Blocks::WALL_SLOPESE5) {
+        return x / 2;
+    } else if (b == Blocks::GROUND_SLOPENE3 || b == Blocks:: GROUND_SLOPESW5
+            || b == Blocks::WALL_SLOPENE3 || b == Blocks:: WALL_SLOPESW5) {
+        return (BLOCK_SIZE - x) / 2;
+    } else if (b == Blocks::GROUND_SLOPENW5 || b == Blocks::GROUND_SLOPESE3
+            || b == Blocks::WALL_SLOPENW5 || b == Blocks::WALL_SLOPESE3) {
+        return (BLOCK_SIZE + x) / 2;
+    } else if (b == Blocks::GROUND_SLOPENE5 || b == Blocks::GROUND_SLOPESW3
+            || b == Blocks::WALL_SLOPENE5 || b == Blocks::WALL_SLOPESW3) {
+        return BLOCK_SIZE - (x / 2);
+    } else if (b == Blocks::GROUND_SLOPENW4 || b == Blocks::WALL_SLOPENW4) {
+        return x < BLOCK_SIZE / 2 ? 2 * x : BLOCK_SIZE;
+    } else if (b == Blocks::GROUND_SLOPENE4 || b == Blocks::WALL_SLOPENE4) {
+        return x <= BLOCK_SIZE / 2 ? BLOCK_SIZE : (BLOCK_SIZE - x) * 2;
+    } else if (b == Blocks::GROUND_SLOPESW4 || b == Blocks::WALL_SLOPESW4) {
+        return x < BLOCK_SIZE / 2 ? BLOCK_SIZE - 2 * x : 0;
+    } else if (b == Blocks::GROUND_SLOPESE4 || b == Blocks::WALL_SLOPESE4) {
+        return x < BLOCK_SIZE / 2 ? 0 :  2 * x - BLOCK_SIZE;
+    } else if (b == Blocks::GROUND_SLOPENW2 || b == Blocks :: WALL_SLOPENW2) {
+        return x < BLOCK_SIZE / 2 ? -5000 : 2 * x - BLOCK_SIZE;
+    } else if (b == Blocks::GROUND_SLOPENE2 || b == Blocks::WALL_SLOPENE2) {
+        return x < BLOCK_SIZE / 2 ? BLOCK_SIZE - 2 * x : -5000;
+    } else if (b == Blocks::GROUND_SLOPESW2 || b == Blocks::WALL_SLOPESW2) {
+        return x < BLOCK_SIZE / 2 ? 5000 : (BLOCK_SIZE - x) * 2;
+    } else if (b == Blocks::GROUND_SLOPESE2 || b == Blocks::WALL_SLOPESE2) {
+        return x < BLOCK_SIZE / 2 ? BLOCK_SIZE - 2 * x : 5000;
+    }
+}
--- src/model/environment/Blocks.h
+++ src/model/environment/Blocks.h
@@ -12,10 +12,57 @@ public:
     enum Blck {
         AIR = 0,
         GROUND = 1,
-        WALL = 2,
+        GROUND_SLOPENW1 = 2,
+        GROUND_SLOPENW2 = 3,
+        GROUND_SLOPENW3 = 4,
+        GROUND_SLOPENW4 = 5,
+        GROUND_SLOPENW5 = 6,
+        GROUND_SLOPENE1 = 7,
+        GROUND_SLOPENE2 = 8,
+        GROUND_SLOPENE3 = 9,
+        GROUND_SLOPENE4 = 10,
+        GROUND_SLOPENE5 = 11,
+        GROUND_SLOPESW1 = 12,
+        GROUND_SLOPESW2 = 13,
+        GROUND_SLOPESW3 = 14,
+        GROUND_SLOPESW4 = 15,
+        GROUND_SLOPESW5 = 16,
+        GROUND_SLOPESE1 = 17,
+        GROUND_SLOPESE2 = 18,
+        GROUND_SLOPESE3 = 19,
+        GROUND_SLOPESE4 = 20,
+        GROUND_SLOPESE5 = 21,
+        WALL = 22,
+        WALL_SLOPENW1 = 23,
+        WALL_SLOPENW2 = 24,
+        WALL_SLOPENW3 = 25,
+        WALL_SLOPENW4 = 26,
+        WALL_SLOPENW5 = 27,
+        WALL_SLOPENE1 = 28,
+        WALL_SLOPENE2 = 29,
+        WALL_SLOPENE3 = 30,
+        WALL_SLOPENE4 = 31,
+        WALL_SLOPENE5 = 32,
+        WALL_SLOPESW1 = 33,
+        WALL_SLOPESW2 = 34,
+        WALL_SLOPESW3 = 35,
+        WALL_SLOPESW4 = 36,
+        WALL_SLOPESW5 = 37,
+        WALL_SLOPESE1 = 38,
+        WALL_SLOPESE2 = 39,
+        WALL_SLOPESE3 = 40,
+        WALL_SLOPESE4 = 41,
+        WALL_SLOPESE5 = 42,
 
-        NBR_BLOCKS = WALL + 1
+        NBR_BLOCKS = WALL_SLOPESE5 + 1
     };
+
+    bool isGround(block b);
+    bool isWall(block b);
+    bool isUp(block b);
+    bool isDown(block b);
+    bool isSlope(block b);
+    int slope(int x, block b);
 
 private:
     Blocks() = default;
